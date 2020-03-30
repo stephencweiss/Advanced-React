c9e4f9df0028829fa9ce27bdaaf85dcf16861cb6
Stephen <stephen.c.weiss@gmail.com>
2020-03-30 09:30:22 -0500

    feat: styled logo
    a logo that will take us to our home page - more importantly though, it's styled.
    
    the styling includes the main attributes we want for the header, its internal anchor
    
    it also has a media query because when we shrink the window, we want the logo to be centered on the page.

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,9 +1,38 @@
 import { Nav } from ".";
+import Link from "next/link";
+import styled from "styled-components";
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-11deg);
+
+  a {
+    padding: 0.5rem 1rem;
+    background: ${({ theme }) => theme.red};
+    color: white;
+    text-transfrom: upper;
+    text-decoration: none;
+  }
+
+  @media( max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`;
+
+
 export function Header() {
   return (
     <div>
       <div className="bar">
-        <a href="">Sick Fits</a>
+        <Logo>
+          <Link href="/">
+            <a>Sick Fits</a>
+          </Link>
+        </Logo>
         <Nav />
       </div>
       <div className="sub-bar">
