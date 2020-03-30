8f90067bce1a3784e9516e3a1e2a9c053cc85fa7
Stephen <stephen.c.weiss@gmail.com>
2020-03-30 08:54:37 -0500

    feat: import meta into page
    by bringing the Meta tag into our page component, we wrap our entire app with our meta settings

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Header } from '.'
+import { Header, Meta } from '.'
 
 export function Page(props) {
   return (
     <div>
+      <Meta />
       <Header />
       {props.children}
     </div>
--- sick-fits/frontend/components/index.js
+++ sick-fits/frontend/components/index.js
@@ -1,3 +1,4 @@
 export * from './Page'
 export * from './Header'
 export * from './Nav'
+export * from './Meta'
