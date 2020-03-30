2a4f14b14902f09268de3e67444c95debaedccd3
Stephen <stephen.c.weiss@gmail.com>
2020-03-30 08:47:11 -0500

    feat: add a header component

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <div>
+      <div className="bar">
+        <a href="">Sick Fits</a>
+      </div>
+      <div className="sub-bar">
+        <p>Search</p>
+      </div>
+      <div>Cart</div>
+    </div>
+  );
+}
