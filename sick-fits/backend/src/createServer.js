8e5a029efdfedceac6d5947057c8dde5c20d10b7
Stephen <stephen.c.weiss@gmail.com>
2020-04-02 18:35:19 -0500

    style: linting

--- sick-fits/backend/src/createServer.js
+++ sick-fits/backend/src/createServer.js
@@ -1,6 +1,6 @@
 const { GraphQLServer } = require("graphql-yoga");
-const  Mutations = require("./resolvers/Mutation");
-const  Query = require("./resolvers/Query");
+const Mutations = require("./resolvers/Mutation");
+const Query = require("./resolvers/Query");
 const db = require("./db");
 
 // Create the GraphQL Yoga Server
