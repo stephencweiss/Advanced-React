e16a9c090ab6f870e0b89033f25f1e15ca4b0d31
Stephen <stephen.c.weiss@gmail.com>
2020-04-02 19:04:46 -0500

    feat: adding queries and mutations
    the first mutations and queries for the yoga server

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,7 @@
-const Query = {};
+const Query = {
+    dogs: function(parent, args, ctx, info){
+        return [{name: 'Snickers', breed: 'GSP'}, {name: 'Finn', breed: 'Lab/Pitt mix'}]
+    }
+};
 
 module.exports = Query;
