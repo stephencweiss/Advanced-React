f7c1bf259f8e25736a26dc32d95f6987b2a50956
Stephen <stephen.c.weiss@gmail.com>
2020-04-08 12:33:50 -0500

    feat: forward query
    instead of replicating logic between yoga and prisma, use prisma-binding
    to forward the query along.

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,8 +1,7 @@
+const {forwardTo} = require('prisma-binding')
+
 const Query = {
-  async items(parent, args, ctx, info) {
-    const items = await ctx.db.query.items()
-    return items;
-  }
+  items: forwardTo('db')
 };
 
 module.exports = Query;
