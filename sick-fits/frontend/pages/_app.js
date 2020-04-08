0d80f289da33dd8fac2bf9138e631f1c62c75e87
Stephen <stephen.c.weiss@gmail.com>
2020-04-08 13:39:11 -0500

    feat: create a shop page that mirrors home

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -4,16 +4,26 @@ import { ApolloProvider } from "react-apollo";
 import withData from "../lib/withData";
 
 function App(props) {
-  const { apollo, Component } = props;
+  const { apollo, Component, pageProps } = props;
   return (
     <Container>
       <ApolloProvider client={apollo}>
         <Page>
-          <Component />
+          <Component {...pageProps} />
         </Page>
       </ApolloProvider>
     </Container>
   );
 }
+
+App.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  // expose the query to the user
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
 
 export default withData(App);
