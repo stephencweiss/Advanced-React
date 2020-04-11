b37cad8d2c162114bba4682086c7d5594903e080
Stephen <stephen.c.weiss@gmail.com>
2020-04-11 10:47:01 -0500

    feat: map over items to render Item

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import styled from "styled-components";
+import Item from './Item'
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -15,19 +17,32 @@ const ALL_ITEMS_QUERY = gql`
   }
 `;
 
+const Center = styled.div`
+  text-align: center;
+`;
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${(theme) => theme.maxWidth};
+  margin: 0 auto;
+`;
 
 // TODO: Update react-apollo to use useQuery instead
 export const Items = (props) => {
   return (
-    <div>
-      <h1>Items!</h1>
+    <Center>
       <Query query={ALL_ITEMS_QUERY}>
-        {({data, error, loading}) => {
-          if(loading) return <p>Loading...</p>
-          if(error) return <p>Error: {error.message}</p>
-          return <p>I found {data.items.length} items!</p>;
+        {({ data, error, loading }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error: {error.message}</p>;
+          return (
+            <ItemsList>
+              {data.items.map((item) => <Item key={item.id} item={item}/>)}
+            </ItemsList>
+          );
         }}
       </Query>
-    </div>
+    </Center>
   );
 };
