0ff61fc032f52e4ffef379f080cb944460741870
Stephen <stephen.c.weiss@gmail.com>
2020-04-11 10:24:30 -0500

    feat: add render props for Query

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,6 +1,33 @@
 import React from "react";
 import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
+
+
+// TODO: Update react-apollo to use useQuery instead
 export const Items = (props) => {
-    return (<div>Item</div>)
-}
+  return (
+    <div>
+      <h1>Items!</h1>
+      <Query query={ALL_ITEMS_QUERY}>
+        {({data, error, loading}) => {
+          if(loading) return <p>Loading...</p>
+          if(error) return <p>Error: {error.message}</p>
+          return <p>I found {data.items.length} items!</p>;
+        }}
+      </Query>
+    </div>
+  );
+};
