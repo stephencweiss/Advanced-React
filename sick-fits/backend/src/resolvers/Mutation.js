e9c938a65f0e05e4b48f36c80df746607e40003d
Stephen <stephen.c.weiss@gmail.com>
2020-04-14 16:44:27 -0500

    style: linting

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,20 +1,25 @@
 const Mutations = {
-  createItem(parent, args, ctx, info){
+  createItem(parent, args, ctx, info) {
     // TODO: Auth check
-    return ctx.db.mutation.createItem({
-      data: {...args}
-    }, info)
+    return ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
   },
-  updateItem(parent, args, ctx, info){
+  updateItem(parent, args, ctx, info) {
     // TODO: Auth check
-    const data = {...args} // needs to be in ItemUpdateInput shape
-    delete data.id
-    return ctx.db.mutation.updateItem({
-      data,
-      where: {id: args.id}
-    }, info)
+    const data = { ...args }; // needs to be in ItemUpdateInput shape
+    delete data.id;
+    return ctx.db.mutation.updateItem(
+      {
+        data,
+        where: { id: args.id }
+      },
+      info
+    );
   }
-
 };
 
 module.exports = Mutations;
