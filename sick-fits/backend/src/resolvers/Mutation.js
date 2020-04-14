00a42901c31f965ede9b68e84082a1be65d04653
Stephen <stephen.c.weiss@gmail.com>
2020-04-14 16:42:53 -0500

    feat: resolver to update item
    the important part here is that the where is an object. if it's not
    properly set GraphQL will error:
    > Error: GraphQL error: Variable "$_v1_where" got invalid value "x";
    > Expected type to be an object
    
    The _v1_ is because it's the second element in the object. It'd be v0
    if it was first.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,20 @@
 const Mutations = {
   createItem(parent, args, ctx, info){
     // TODO: Auth check
-    console.log({args, info})
     return ctx.db.mutation.createItem({
       data: {...args}
     }, info)
+  },
+  updateItem(parent, args, ctx, info){
+    // TODO: Auth check
+    const data = {...args} // needs to be in ItemUpdateInput shape
+    delete data.id
+    return ctx.db.mutation.updateItem({
+      data,
+      where: {id: args.id}
+    }, info)
   }
+
 };
 
 module.exports = Mutations;
