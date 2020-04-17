26f33791c1e127ff2409358fe230f6dee9da4f7b
Stephen <stephen.c.weiss@gmail.com>
2020-04-17 16:10:03 -0500

    feat: use DeleteItem component

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -5,6 +5,7 @@ import ItemStyles from "./styles/ItemStyles";
 import Title from "./styles/Title";
 import PriceTag from "./styles/PriceTag";
 import formatMoney from "../lib/formatMoney";
+import {DeleteItem} from './DeleteItem'
 
 const Item = ({ item }) => {
   return (
@@ -22,7 +23,7 @@ const Item = ({ item }) => {
           <a>Edit</a>
         </Link>
         <button> Add To Cart </button>
-        <button> Delete </button>
+        <DeleteItem id={item.id}> Delete this item! </DeleteItem>
       </div>
     </ItemStyles>
   );
