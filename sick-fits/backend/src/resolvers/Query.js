35e65e16ec669220acb083352f747fbd0bf94f5f
Stephen <stephen.c.weiss@gmail.com>
2020-04-26 15:34:53 -0500

    feat: add an identity query to fetch user
    when a user is logged in, they should be able to get their identity.
    wes called this a "me" query, but I found that less communicative.

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -3,7 +3,19 @@ const { forwardTo } = require("prisma-binding");
 const Query = {
   items: forwardTo("db"),
   item: forwardTo("db"),
-  itemsConnection: forwardTo("db")
+  itemsConnection: forwardTo("db"),
+  identity: function identity(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+
+    // returns the user based on the userId, else null
+    if (!userId) return null;
+    return ctx.db.query.user(
+      {
+        where: { id: userId }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
