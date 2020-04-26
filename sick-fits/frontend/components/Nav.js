a32774ce5b19cad17f79196295de5239611e61fc
Stephen <stephen.c.weiss@gmail.com>
2020-04-26 16:53:23 -0500

    feat: nav context dependent
    if a user is logged in / out the nav reflects available options

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,28 +3,32 @@ import NavStyles from "./styles/NavStyles";
 import User from "./User";
 export function Nav() {
   return (
-    <NavStyles>
-      <User>
-        {({ data: {identity} }) => {
-          console.log(identity);
-          return <p>{identity && identity.name}</p>;
-        }}
-      </User>
-      <Link href="/items">
-        <a>Shop</a>
-      </Link>
-      <Link href="/sell">
-        <a>Sell</a>
-      </Link>
-      <Link href="/signup">
-        <a>Signup</a>
-      </Link>
-      <Link href="/orders">
-        <a>Orders</a>
-      </Link>
-      <Link href="/me">
-        <a>Account</a>
-      </Link>
-    </NavStyles>
+    <User>
+      {({ data: { identity } }) => (
+        <NavStyles>
+          <Link href="/items">
+            <a>Shop</a>
+          </Link>
+          {identity && (
+            <>
+              <Link href="/sell">
+                <a>Sell</a>
+              </Link>
+              <Link href="/orders">
+                <a>Orders</a>
+              </Link>
+              <Link href="/me">
+                <a>Account</a>
+              </Link>
+            </>
+          )}
+          {!identity && (
+            <Link href="/signup">
+              <a>Sign In</a>
+            </Link>
+          )}
+        </NavStyles>
+      )}
+    </User>
   );
 }
