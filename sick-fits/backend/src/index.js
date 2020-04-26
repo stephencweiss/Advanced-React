a40c9c4992d93abfd3102c0e2434ac666b2addd7
Stephen <stephen.c.weiss@gmail.com>
2020-04-26 15:25:07 -0500

    feat: middleware to pull out userId from jwt

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,10 +1,21 @@
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const createServer = require("./createServer");
 
 const server = createServer();
 
-server.express.use(cookieParser())
+server.express.use(cookieParser());
+
+// decode the JWT so that we can get the user id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 // TODO: Use express middleware to populate current user
 
 server.start(
